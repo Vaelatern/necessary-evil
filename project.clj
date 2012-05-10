6c6f3bc8750a4938f89213227d8f5708c3eae33f
Andrew Brehaut <andrew@brehaut.net>
2012-05-10 14:51:45 +1200

    changed version range to ensure that clojure isnt clamped at 1.3.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :repositories {"sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "[1.2.1],[1.3.0]"]
+  :dependencies [[org.clojure/clojure "[1.2.1,)"]
                  [org.clojure/data.zip "0.1.0"]
                  [org.clojure/algo.monads "0.1.1-SNAPSHOT"]
                  [clj-http "0.2.7"]

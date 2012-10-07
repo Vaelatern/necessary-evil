7c3d716fd10a475f32e91509aebcfb746c497389
Andrew Brehaut <andrew@brehaut.net>
2012-10-08 12:24:55 +1300

    uses latest algo.monads snapshot

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies {org.clojure/clojure "[1.2.1,)",
                  org.clojure/data.zip "0.1.0",
-                 org.clojure/algo.monads "0.1.1-SNAPSHOT",
+                 org.clojure/algo.monads "0.1.3-SNAPSHOT",
                  clj-http "0.2.7",
                  commons-codec "1.4",
                  clj-time "0.4.4",
